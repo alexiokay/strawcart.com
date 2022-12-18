47d6e691cdfc0fac1ec2fbff7db3ab1e8db277d9
papadi166 <papadi166@gmail.com>
2022-12-18 20:41:22 +0100

    small fix

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@ div(class=" w-full h-full bg-transparent flex flex-col mt-[0.35rem] px-4 ")
   div(class="flex flex-col md:flex-row text-center md:text-start w-full justify-around items-center h-auto mt-16")
     div(class="z-20 block relative h-[24rem] md:h-[28rem] w-max rounded-xl md:hover:scale-125 order-0 md:order-1")
       img(class=" w-max object-contain drop-shadow-xl rounded-xl border-2   border-[#DADADA]  h-full" src="/images/strawcart-model.jpg")
-      img(class="absolute top-0 right-[-65px] w-[8rem] md:w-[10rem] h-[6rem] md:h-[8rem]" src="/images/1-year-warranty.png")
+      img(class="hidden md:absolute top-0 right-[-65px] w-[8rem] md:w-[10rem] h-[6rem] md:h-[8rem]" src="/images/1-year-warranty.png")
     div(class="flex w-full  md:w-2/5 flex-col  order-1 md:order-0")
       h1(class=" text-3xl mt-10 md:mt-0 md:text-[40px] leading-[138%] font-itim font-inter") {{$t('components.strawcart_showcase.title')}}
       h2(class="leading-[173%] tracking-[2%]  text-lg  md:text-[20px] mt-8") {{$t('components.strawcart_showcase.description')}}
